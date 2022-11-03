b23f766803ed7dc1b705f5ba1277e62ab72ec7f6
brenley-woodard <brenleyew@gmail.com>
2022-11-03 11:57:06 -0700

    have 3 failing tests for add exclamation point

--- test/tests.js
+++ test/tests.js
@@ -21,8 +21,14 @@ skip('this test should be skipped', (expect) => {
 
 test('it should add three exclamation points', (expect) => {
     const expected = 'bunny rabbit!!!';
-
     const actual = addExclamationPoints('bunny rabbit');
-
     expect.equal(actual, expected);
+
+    const expected2 = 'blue whale!!!';
+    const actual2 = addExclamationPoints('blue whale');
+    expect.equal(actual2, expected2);
+
+    const expected3 = 'cats!!!';
+    const actual3 = addExclamationPoints('cats');
+    expect.equal(actual3, expected3);
 });
